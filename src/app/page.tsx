21039ea2081ac02915456bb189206c021bdf3621
mario-leandro <marioleandro2004@gmail.com>
2025-03-03 17:24:54 -0300

    Preparando os componentes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
-import Image from "next/image";
+import Header from "@/components/Header";
+import Main from "@/components/Main";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <h1>Hello World!</h1>
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
   );
 }
