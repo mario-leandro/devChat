a3acf74a393f198d97857b4eec3f00fd94849f9f
mario-leandro <marioleandro2004@gmail.com>
2025-06-16 22:32:38 -0300

    Ajustando responsividade da tela - por enquanto so para telas grandes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,12 @@ import Main from "@/components/Main";
 
 export default function Home() {
   return (
-    <section className="w-dvw h-dvh display flex justify-center items-center">
-      <section className="w-7xl h-11/12 flex justify-start items-start bg-slate-900 rounded-2xl">
-        <Navigation />
-        <Main />
+    <section className="w-screen h-screen display flex justify-center items-center">
+      <section className="max-w-7xl h-[85%]">
+        <section className="w-full h-full flex justify-start items-start bg-slate-900 rounded-2xl">
+          <Navigation />
+          <Main />
+        </section>
       </section>
     </section>
   );
